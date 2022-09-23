b589d839d8cdadf5b81a2996ea72215cf8520460
fpiskur <filip.piskur@gmail.com>
2022-09-23 20:22:26 +0200

    Add user login form, layout and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/login', to: 'sessions#new'
+  
+  root "users#new"
+  
 end

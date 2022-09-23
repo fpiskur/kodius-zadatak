7d565d3f36b2e0f257838d4263acdf6f3dde4c39
fpiskur <filip.piskur@gmail.com>
2022-09-23 22:25:13 +0200

    Enable users to log in with remembering, add user sign up form

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,54 @@
 module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+    session[:session_token] = user.session_token
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns the user corresponding to the remember token cookie
+  def current_user
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # Returns true if the given user is the current user.
+  # def current_user?(user)
+  #   user && user == current_user
+  # end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    reset_session
+    @current_user = nil
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  get '/login', to: 'sessions#new'
-  
-  root "users#new"
-  
+  root "sessions#new"
+  get    '/login',     to: 'sessions#new'
+  post   '/login',     to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
+  get    '/signup',    to: 'users#new'
+
+  resources :users
+
 end

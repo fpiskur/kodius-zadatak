b57743aa8bf660896d608572e58d4d6045aac3ca
fpiskur <filip.piskur@gmail.com>
2022-09-23 23:30:44 +0200

    Add create user action, setup mailers for account activation and password reset

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
       end
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,40 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy]
+
   def new
     @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
+
+    # Confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
+
+    # Confirms an admin user
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 
 end

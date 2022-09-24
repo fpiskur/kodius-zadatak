e319fb83c1c20ef8c23594370e0f35a3d67573c8
fpiskur <filip.piskur@gmail.com>
2022-09-24 18:42:58 +0200

    Add seed data for reservations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-
+# Create users
 User.create!(
   name: "Admin User",
   email: "filip.piskur@gmail.com",
@@ -21,6 +21,7 @@ User.create!(
   activated_at: Time.zone.now
 )
 
+# Create rooms
 1.upto(5) do |n|
   1.upto(5) do |m|
     number = (n.to_s + m.to_s).to_i
@@ -33,3 +34,19 @@ User.create!(
                   description: description)
   end
 end
+
+# Create reservations
+Reservation.create!(room_id: 1,
+                    user_id: 2,
+                    check_in_at: Date.today.to_fs,
+                    check_out_at: (Date.today + 5).to_fs)
+
+Reservation.create!(room_id: 6,
+                    user_id: 2,
+                    check_in_at: Date.yesterday.to_fs,
+                    check_out_at: Date.today.to_fs)
+
+Reservation.create!(room_id: 12,
+                    user_id: 2,
+                    check_in_at: (Date.today + 8).to_fs,
+                    check_out_at: (Date.today + 13).to_fs)

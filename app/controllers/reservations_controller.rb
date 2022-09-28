86aa5df536318b33726ad0f8e8e100661ad49934
fpiskur <filip.piskur@gmail.com>
2022-09-28 22:00:12 +0200

    Move reservation_available validation to reservation model

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,7 +2,7 @@ class ReservationsController < ApplicationController
 
   before_action :logged_in_user
   before_action :get_room,          only: [:create]
-  before_action :reservation_available, only: [:create]
+  # before_action :reservation_available, only: [:create]
 
   def create
     @reservation = @room.reservations.build(reservation_params)
@@ -24,18 +24,6 @@ class ReservationsController < ApplicationController
 
     def get_room
       @room = Room.find(params[:room_id])
-    end
-
-    def reservation_available
-      # Get any overlaps
-      sql = ":check_out_at >= check_in_at AND check_out_at > :check_in_at"
-      reservations = @room.reservations.where(sql,
-              check_out_at: params[:reservation][:check_out_at], check_in_at: params[:reservation][:check_in_at])
-      # In case there are overlaps display flash message
-      if reservations.any?
-        flash[:danger] = "Those dates are not available for reservation"
-        redirect_to room_path(@room)
-      end
     end
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -10,5 +10,18 @@ class Reservation < ApplicationRecord
   validates :check_out_at, comparison: { greater_than: :check_in_at }
   validates :check_in_at, comparison: { greater_than_or_equal_to: Date.today,
                           message: "- Check in date cannot be in the past" }
+
+  validate :reservation_available
+
+  private
+
+    def reservation_available
+      # Get any overlaps
+      sql = ":check_out_at >= check_in_at AND check_out_at > :check_in_at"
+      reservations = room.reservations.where(sql,
+              check_out_at: check_out_at, check_in_at: check_in_at)
+      return if reservations.none?
+      errors.add(:base, "Those dates are not available for reservation")
+    end
   
 end
